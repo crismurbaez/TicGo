14f41cf8fd6b6ef1e992136fefe574f810a4d473
Cristina <clmbjcm@gmail.com>
2023-03-24 21:56:46 -0300

    configure tailwind css and copy  navbar template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './Components/Navbar/NavBar';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {/* <Provider store={store}> --> cuando agregue redux*/}
+      <Routes>
+        <Route
+          path={"/"}
+          element={<NavBar />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route
+            path={"Home"}
+            element={<Home />}
+          />
+          <Route
+            path='*'
+            element={
+              <h1>404</h1>
+            }
+          />
+        </Route>
+      </Routes>
+      {/* </Provider> */}
+    </div >
   );
 }
 
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <span> Este es el Home</span>
+
+        </div>
+    )
+};
+
+export default Home;
